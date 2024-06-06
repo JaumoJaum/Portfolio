eab312e1e59ad519b44030bd3c0479b635b8c51e
João Pedro Grippa <jp.mrgp@gmail.com>
2024-06-06 08:36:47 -0300

    update day 6

--- site/src/script/script.js
+++ site/src/script/script.js
@@ -1,16 +1,21 @@
-
-
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
 
-        const target = document.querySelector(this.getAttribute('href'));
-        const offset = 60;
-        const topPos = target.getBoundingClientRect().top + window.pageYOffset - offset;
+        // Verifica se o href é "#Home"
+        if (this.getAttribute('href') === '#Home') {
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            });
+        } else {
+            const target = document.querySelector(this.getAttribute('href'));
+            const topPos = target.getBoundingClientRect().top + window.pageYOffset;
 
-        window.scrollTo({
-            top: topPos,
-            behavior: 'smooth'
-        });
+            window.scrollTo({
+                top: topPos,
+                behavior: 'smooth'
+            });
+        }
     });
 });
